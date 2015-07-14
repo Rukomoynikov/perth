cec01d3040fb13fc334d3b3dbbaba38cceeffbdb
Maksim Rukomoynikov <cat@mos-it.com>
2015-07-14 13:53:42 +0300

    Выпадающее меню

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,7 +4,7 @@ var menu_button = document.querySelector(".open-menu");
 menu_button.addEventListener('click', openMenu);
 
 var close_menu_button = document.querySelector(".close-menu");
-menu_button.addEventListener('click', closeMenu);
+close_menu_button.addEventListener('click', closeMenu);
 
 var menu_block = document.querySelector("section.menu");
 
@@ -16,5 +16,4 @@ function openMenu (event){
 function closeMenu (event){
 	// event.stopImmediatePropagation()
 	menu_block.style.display = 'none';
-	console.log('close');
 }
